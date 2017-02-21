64f244b962a1f347040aa2570961045114c3a76d
Stephen Niedzielski <stephen@niedzielski.com>
2017-02-20 18:18:09 -0700

    chore: fix lints

--- src/js/Game.js
+++ src/js/Game.js
@@ -2,20 +2,28 @@ export default class Game {
   init() {
     const canvas = document.querySelector('canvas')
 
-    function draw() {
+    const draw = () => {
       const ctx = canvas.getContext('2d')
 
       const lvl = '--\\----.------------------------------'
       let x = 0
-      let y = 50
+      let y = 50 // eslint-disable-line no-magic-numbers
       ctx.beginPath()
       ctx.moveTo(x, y)
 
       for (const segment of lvl) {
         switch (segment) {
-          case '-': x += 10; break
-          case '.': y += 10; break;
-          case '\\': x += 10; y += 10; break;
+          case '-':
+            x += 10
+            break
+          case '.':
+            y += 10
+            break
+          case '\\':
+            x += 10
+            y += 10
+            break
+          default: break
         }
         ctx.lineTo(x, y)
       }
@@ -23,8 +31,8 @@ export default class Game {
       ctx.stroke()
     }
 
-    function resize() {
-      console.log(`${canvas.width}`)
+    const resize = () => {
+      // console.log(`${canvas.width}`)
       const ratio = window.innerWidth / window.innerHeight
       canvas.width = 100 * ratio
       canvas.height = 100
@@ -37,14 +45,15 @@ export default class Game {
 
     const fps = 0
     const loop = () => {
-      ///console.log('hello from the other side')
-      var tNow = /*window.*/performance.now();
+      // console.log('hello from the other side')
+      // const tNow = /*window.*/performance.now();
       // const delta = 0 // time since last draw
       // update(delta)
 
       draw()
     }
 
-    /*window.*/setInterval(loop, fps > 0 ? 1000 / fps : 0)
+    const millisPerSec = 1000
+    /* window.*/setInterval(loop, fps > 0 ? millisPerSec / fps : 0)
   }
 }
